07db3f813cfe91b3cad08f603377ca5d415fb360
Emmax <Emmax.uwujare@hotmail.com>
2025-04-28 13:30:59 -0400

    added test case done by Achyuth due to pushing error he is having

--- frontend/cypress/e2e/logout.cy.js
+++ frontend/cypress/e2e/logout.cy.js
@@ -1,20 +1,21 @@
-describe('Logout Functionality', () => {
-    it('should logout and navigate to login', () => {
-      cy.visit('http://localhost:5173/resume_templates');
-  
-      // Simulate user already logged in
-      window.localStorage.setItem('isLoggedIn', 'true');
-  
-      // Click Logout button
-      cy.get('button').contains('Logout').click();
-  
-      // After logout, should navigate to login
-      cy.url().should('include', '/login');
-    });
+/// <reference types="cypress" />
+/* global cy, describe, beforeEach  it */
+describe("Logout Functionality", () => {
+  it("should logout and navigate to login", () => {
+    cy.visit("http://localhost:5173/resume_templates");
+
+    // Simulate user already logged in
+    window.localStorage.setItem("isLoggedIn", "true");
+
+    // Click Logout button
+    cy.get("button").contains("Logout").click();
+
+    // After logout, should navigate to login
+    cy.url().should("include", "/login");
   });
-  beforeEach(() => {
-    // Clear localStorage/sessionStorage before each test
-    cy.clearLocalStorage();
-    cy.clearCookies();
-  });
-  
+});
+beforeEach(() => {
+  // Clear localStorage/sessionStorage before each test
+  cy.clearLocalStorage();
+  cy.clearCookies();
+});
